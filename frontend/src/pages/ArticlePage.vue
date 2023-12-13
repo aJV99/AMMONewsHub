6ea0082bd4fb970bc14b01edf1810c19f7a58c20
oryna <ec20251@qmul.ac.uk>
2023-12-13 17:01:48 +0000

    comment styling done

--- frontend/src/pages/ArticlePage.vue
+++ frontend/src/pages/ArticlePage.vue
@@ -1,87 +1,51 @@
 <template>
-  <div class="h1">
-    {{ title }}
+  <div class="h1 px-3">
+    {{article?.Article_title}}
   </div>
-  <p>{{article?.Article_title}}</p>
-  <p>{{article?.Article_summary}}</p>
-  <!-- <div v-if="article?.Comments !== null"></div> -->
-    <div v-for="comment in comments" class="container border border-dark text-center mb-5 w-25">
-        <!-- <p>{{comment}}</p> -->
+  <div class="bg-new p-2 m-2 mb-3 ">
+  <p>{{article?.Article_summary}} Great Britain's most successful female Olympian Dame Laura Kenny believes women athletes can struggle to get pregnant because of their lifestyle.
+
+Kenny, 31, won five gold medals and one silver across three Olympic Games at London 2012, Rio 2016 and Tokyo 2020.
+
+The mum-of-two is targeting a fourth Games in Paris next year.
+
+Kenny says she has had "many conversations" with athletes suffering with Relative Energy Deficiency in Sport.
+
+Known as Red-S, the condition occurs when athletes expend more energy in training than they consume through food and drink and one of the key symptoms in women is the absence of periods.
+
+5 Live Sport podcast: All About - The Laura and Jason Kenny one
+Several sportswomen have spoken about their experience with Red-S, including athletes Anna Boniface, Bobby Clay and Pippa Woolven and mountain bike racer Evie Richards.
+
+"There are females that have struggled and will struggle to get pregnant because of the lifestyle of being an athlete," Kenny, who does not have the condition, told BBC Radio 5 Live Breakfast.
+
+"We've all heard of Red-S - being females losing their periods. You're not going to be able to fall pregnant if you haven't got a period.
+
+"It's actually a really unhealthy lifestyle that these females can't have kids and it's actually really sad.
+
+"I've always consistently had a period but the amount of conversations I've heard of people having Red-S.
+
+"Red-S is actually really dangerous... these people are giving up lots of things that really deep down they want."
+  </p>
+</div>
+<h4>Comments</h4>
+
+<div>
+  <form action="" class="d-flex flex-row pb-4 pt-3 m-auto justify-content-center">
+    <input
+        class="comment outline-0 bg-light rounded-5 px-4 py-2"
+        placeholder="Enter Comment"
+        required
+        type="text"
+      />
+      <div class="btn align-self-center fw-lighter maroon rounded-pill px-4 py-3 mx-2">Post</div>
+  </form>
+</div>
+
+    <div v-for="comment in comments" class=" mx-auto bottom-comment">
         <Comment :node="comment" />
-
-
-    </div>
-    <p> <button
-            type="button"
-            class="btn px-4 btn-success mx-3"
-            data-bs-toggle="modal"
-            data-bs-target="#exampleModal"
-          >
-            Add Comment
-    </button></p>  
-
-  <!-- Modal For Adding Comment -->
-  <div
-      class="modal fade"
-      id="exampleModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title p-2" id="exampleModalLabel">Add Comment</h4>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body px-5">
-            <form class="" >
-              <!-- @submit="sendData() -->
-              <div class="pb-3">
   
-                <div class="py-2">
-                  <h6>Add Comment:</h6>
-                  <input
-                    id="comment_content"
-                    v-model="comment_content"
-                    class="border-0 outline-0 bg-light rounded-5 px-4 py-2 w-100"
-                    placeholder="Text"
-                    required
-                    type="text"
-                  />
-                </div>
-              </div>
-              <div class="d-flex justify-content-end">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button
-                  type="submit"
-                  class="btn btn-primary mx-2"
-                >
-                  Save changes
-                </button>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer"></div>
-        </div>
-      </div>
     </div>
-
- 
-
-
-   
+   <div class="mb-5"></div>
 </template>
 
 <script lang="ts">
