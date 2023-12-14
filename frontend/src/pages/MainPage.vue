35d94a92a6a5e0d6648c7eec265093dc897eee8a
Marco Solari <m.solari@se20.qmul.ac.uk>
2023-12-14 21:15:13 +0000

    filter

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -2,49 +2,71 @@
   <div class="h1">
     {{ title }}
   </div>
-  <select class="form-select" v-model="filter">
-    <option :value="null">Choose Category</option>
-    <option v-for="article in Articles" :value="article.Article_category">{{ article.Article_category }} </option>
+  {{ console.log("profile.favorite_categories") }}
 
-  </select>
-  <p>{{ console.log(filter) }}</p>
-  <div v-if="filter !== null">
-    <div v-for="article in Articles" :key="article.id" class="container text-center  w-25">
-      
-        <div v-if="filter == article.Article_category" class="mb-5 border border-dark">
+  <div v-if="profile">
+    <div v-if="profile.favorite_categories.length !== 0">
+      {{ console.log(profile.favorite_categories) }}
+      <div v-for="article in Articles" :key="article.id" >
+        <div v-if="profile.favorite_categories.includes(article.Article_category)" class="container border border-dark text-center mb-5 w-25">
+        <router-link :to="`/article/${article.id}`">
           <p>{{ article.id }}</p>
           <p class="font-weight-bold">{{ article.Article_title }}</p>
           <p>{{ article.Article_summary }}</p>
           <p>{{ article.Article_category }}</p>
-          <p class="font-italic">{{ article.Article_date }}</p>  
+          <p class="font-italic">{{ article.Article_date }}</p>
+        </router-link>
         </div>
       </div>
-  </div>
-
-  <div v-else>
-    <div v-for="article in Articles" :key="article.id" class="container border border-dark text-center mb-5 w-25">
-      <router-link :to="`/article/${article.id}`">
-        <p>{{ article.id }}</p>
-        <p class="font-weight-bold">{{ article.Article_title }}</p>
-        <p>{{ article.Article_summary }}</p>
-        <p>{{ article.Article_category }}</p>
-        <p class="font-italic">{{ article.Article_date }}</p>
-      </router-link>
     </div>
+    <div v-else>
+      <div v-for="article in Articles" :key="article.id" class="container border border-dark text-center mb-5 w-25">
+        <router-link :to="`/article/${article.id}`">
+          <p>{{ article.id }}</p>
+          <p class="font-weight-bold">{{ article.Article_title }}</p>
+          <p>{{ article.Article_summary }}</p>
+          <p>{{ article.Article_category }}</p>
+          <p class="font-italic">{{ article.Article_date }}</p>
+        </router-link>
+        </div>
+  </div> 
   </div>
-   
+  <div v-else>
+      <div v-for="article in Articles" :key="article.id" class="container border border-dark text-center mb-5 w-25">
+        <router-link :to="`/article/${article.id}`">
+          <p>{{ article.id }}</p>
+          <p class="font-weight-bold">{{ article.Article_title }}</p>
+          <p>{{ article.Article_summary }}</p>
+          <p>{{ article.Article_category }}</p>
+          <p class="font-italic">{{ article.Article_date }}</p>
+        </router-link>
+        </div>
+  </div> 
+  
+
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
 import { useArticleStore } from '../stores/articleStore';
+import { useUserProfileStore } from '../stores/userProfile';
+
 
 export default defineComponent({
   setup() {
     const articleStore = useArticleStore();
+    const userProfileStore = useUserProfileStore();
+    const profile = ref(userProfileStore.profile);
+
+    onMounted(async () => {
+              if (!userProfileStore.profile) {
+                  await userProfileStore.fetchUserProfile();
+              }
+          });
 
     return {
       Articles: articleStore.Articles,
+      profile,
       title: "Main Page",
     };
   },
